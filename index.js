f92363eb91fa9370b8ca4907a5bc3a75b1932e49
Khomkrit Uparakham <khomkrit@swaple.io>
2019-02-12 17:27:23 +0700

    Parse txparams  from .json file instead  hardcode

--- index.js
+++ index.js
@@ -3,44 +3,10 @@ const privateKey = Buffer.from(
   'e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109',
   'hex'
 )
+var fs = require('fs')
+var obj = JSON.parse(fs.readFileSync('test.json', 'utf8'))
 
-/*
-const txParams = {
-  nonce: '0x00',
-  gasPrice: '0x09184e72a000',
-  gasLimit: '0x2710',
-  to: '0x0000000000000000000000000000000000000000',
-  value: '0x00',
-  from: '0x0000000000000000000000000000000000000000',
-  data: '',
-  // data:
-  //  '0x7f7465737432000000000000000000000000000000000000000000000000000000600057',
-  // EIP 155 chainId - mainnet: 1, ropsten: 3
-  chainId: 42
-}
-*/
-
-var nonce = process.argv[2]
-var gasPrice = process.argv[3]
-var gasLimit = process.argv[4]
-var to = process.argv[5]
-var value = process.argv[6]
-var from = process.argv[7]
-var data = process.argv[8]
-var chainId = process.argv[9]
-
-const txParams = {
-  nonce,
-  gasPrice,
-  gasLimit,
-  to,
-  value,
-  from,
-  data,
-  chainId
-}
-
-const tx = new EthereumTx(txParams)
+const tx = new EthereumTx(obj)
 tx.sign(privateKey)
 const serializedTx = tx.serialize()
 
