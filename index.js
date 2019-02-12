5ca83121effbb98382a76d2376aa9f0ca4f04957
Khomkrit Uparakham <khomkrit@swaple.io>
2019-02-12 18:14:16 +0700

    Refactor to import object instead read from file

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const EthereumTx = require('ethereumjs-tx')
-const privateKey = Buffer.from(
-  'e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109',
-  'hex'
-)
-var fs = require('fs')
-var obj = JSON.parse(fs.readFileSync('test.json', 'utf8'))
+const testData = require('./test.js')
 
-const tx = new EthereumTx(obj)
+const privateKey = new Buffer(testData.privateKey, 'hex')
+
+const tx = new EthereumTx(testData.rawTx)
+
 tx.sign(privateKey)
 const serializedTx = tx.serialize()
 
-console.log(`tx is  ${JSON.stringify(tx)}`)
+//console.log(tx.toJSON())
+//console.log(`tx is  ${JSON.stringify(tx)}`)
 console.log(`serializedTx is ${'0x' + serializedTx.toString('hex')}`)
---
+++ test.js
@@ -0,0 +1,17 @@
+const rawTx = {
+  nonce: '0x01',
+  gasPrice: '0x09184e72a000',
+  gasLimit: '0x2710',
+  to: '0x612226f04538fd002d061678289d9847b76ff15a',
+  value: '0x01',
+  data: '',
+  chainId: 42
+}
+
+const privateKey =
+  '8a53567c58cf2801449667966b244128838096f2d03f032c1a8baa77bdfa34cf'
+
+module.exports = {
+  rawTx,
+  privateKey
+}
